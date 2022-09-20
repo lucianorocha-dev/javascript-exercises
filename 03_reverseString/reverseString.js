9021ace0c765c04c5f071516b3a743e1d4c214dc
Luciano Rocha <52541757+lucianorocha-dev@users.noreply.github.com>
2022-09-20 18:29:01 -0300

    Solve exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let arrWord = word.split("");
+  let arrReverse = [];
+  for (let i = arrWord.length - 1; i >= 0; i--) {
+    arrReverse.push(arrWord[i]);
+  }
+  let strWord = arrReverse.join("");
+  return strWord;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
